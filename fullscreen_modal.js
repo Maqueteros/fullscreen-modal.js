b1328016d5b89530688b8463bc028584add6fa0f
Julio Inzunza <julio.inzunza@amplemind.com>
2013-09-01 22:17:41 -0700

    Confirm added

--- fullscreen_modal.js
+++ fullscreen_modal.js
@@ -1,32 +1,23 @@
-(function(window, $){
+(function(window, $) {
 
   var fs_timeout,
+      modal,
       keyup_enable = false,
-      fs_timeout_counter = 2000,
+      fn = ( $.fn.bind ) ? 'bind' : 'on',
       _fs_modal = function(options) {
 
-        this.options = $.extend({
-          message   : 'Insert text here.',
-          type      : 'info', // [info, success, error, warning]
-          autoclose : true,
-          timeout   : 200
-        }, options);
+        var self = this;
 
-        var elements = create(this.options),
-            fn = ($.fn.bind) ? 'bind' : 'on';
+        self.options = $.extend(self.reset_options, options);
+        modal = create_modal(self.options);
 
-        this.modal = elements.modal;
-
-        this.modal[fn]('click', close);
-        $('body')[fn]('keyup', close);
+        return self;
 
       },
       close = function() {
 
         if( keyup_enable ) {
-          var self = $('.fsModalContainer');
-
-          self.fadeOut(300, function() {
+          modal.fadeOut(300, function() {
             keyup_enable = false;
 
             if( fs_timeout ) {
@@ -36,14 +27,86 @@
         }
 
       },
-      create = function(options) {
-        var modal = $('<div class="fsModalContainer '+ options.type +'"></div>').appendTo('body'),
-            message = $('<p class="fsMessage">'+ options.message +'</p>').appendTo(modal);
+      create_form = function() {
 
-        return {
-          modal: modal
-        };
+        var self = this;
+
+      },
+      create_confirm = function() {
+
+        var buttons_container,
+            self             = this,
+            message          = $('.fsMessage', modal),
+            confirm_button   = $('.fsConfirmButton', modal),
+            cancel_button    = $('.fsCancelButton', modal),
+            middle_container = $('.fsModalMiddle', modal);
+
+        modal.unbind('click').css('cursor', 'default').addClass('confirm');
+        $('body').unbind('keyup');
+
+        if( ! message.length ) {
+          message = $('<p class="fsMessage">' + self.options.message + '</p>').appendTo(middle_container);
+        }
+
+        if( ! confirm_button.length ) {
+          buttons_container = $('<div class="fsButtonsContainer"></div>').appendTo(middle_container);
+          confirm_button    = $('<button class="button fsConfirmButton">Confirmar</button>').appendTo(buttons_container);
+          cancel_button     = $('<button class="button fsCancelButton">Cancelar</button>').appendTo(buttons_container);
+        }
+
+        confirm_button[fn]('click', $.proxy(self.confirm_trigger, self));
+        cancel_button[fn]('click', $.proxy(self.cancel_confirm_trigger, self));
+
+      },
+      create_alert = function() {
+
+        var self  = this,
+            alert = $('.fsMessage', modal);
+
+        modal[fn]('click', close);
+        $('body')[fn]('keyup', self.close);
+
+        if( ! alert.length ) {
+          alert = $('<p class="fsMessage"></p>').find('.fsModalMiddle').appendTo(modal);
+        }
+        alert.html(self.options.message);
+
+        return alert;
+
+      },
+      create_modal = function(options) {
+
+        return $('<div class="fsModalContainer ' + options.type + '"><div class="fsModalMiddle"></div></div>').appendTo('body');
+
+      },
+      set_modal_type = function(modal_type, fsModal) {
+
+        switch(modal_type) {
+          case 'form':
+            create_form.call(fsModal, []);
+            break;
+          case 'alert':
+            create_alert.call(fsModal, []);
+            break;
+          case 'confirm':
+            create_confirm.call(fsModal, []);
+            break;
+        }
+
       };
+
+  _fs_modal.prototype.reset_options = function() {
+    this.options = {
+      message         : 'Insert text here.',
+      type            : 'info', // [info, success, error, warning]
+      autoclose       : true,
+      timeoutFade     : 200,
+      timeoutClose    : 2000,
+      modalType       : 'alert',
+      confirmCallback : false,
+      cancelCallback  : false
+    };
+  };
 
   _fs_modal.prototype.open = function() {
 
@@ -51,38 +114,77 @@
 
     $('input').blur();
 
-    this.modal.fadeIn(300, function() {
-      if( self.options.autoclose ) {
-        fs_timeout = setTimeout(self.close, fs_timeout_counter);
+    set_modal_type(self.options.modalType, self);
+
+    modal.fadeIn(this.timeoutFade, function() {
+      if( self.options.autoclose && self.options.modalType === 'alert' ) {
+        fs_timeout = setTimeout(self.close, self.options.timeoutClose);
       }
+
       keyup_enable = true;
     });
 
   };
 
   _fs_modal.prototype.close = function() {
-    close.call(this.modal, []);
+
+    close.call(modal, []);
+
+  };
+
+  _fs_modal.prototype.confirm_trigger = function(event) {
+
+    var self = this;
+
+    if( self.options.confirmCallback && typeof self.options.confirmCallback === 'function' ) {
+      self.options.confirmCallback();
+    }
+
+    self.close();
+
+  };
+
+  _fs_modal.prototype.cancel_confirm_trigger = function(event) {
+
+    var self = this;
+
+    if( self.options.cancelCallback && typeof self.options.cancelCallback === 'function' ) {
+      self.options.cancelCallback();
+    }
+
+    self.close();
+
   };
 
   _fs_modal.prototype.setOptions = function(options) {
 
-    this.options = $.extend(this.options, options);
+    var self = this;
 
-    $('.fsMessage', this.modal).text(this.options.message)
-                               .parent()
-                               .removeClass('info error warning success')
-                               .addClass(this.options.type);
+    self.reset_options();
+    self.options = $.extend(self.options, options);
+
+    if( modal.find('.fsButtonsContainer').length ) {
+      modal.find('.fsButtonsContainer').remove();
+    }
+
+    $('.fsMessage', modal).html(self.options.message);
+    modal.removeClass('info error warning success confirm').addClass(self.options.type);
 
     if( fs_timeout ) {
       clearTimeout(fs_timeout);
     }
 
-    if( ! this.options.autoclose ) {
+    if( ! self.options.autoclose ) {
       fs_timeout = undefined;
-      if( options.autoclose === undefined ) {
-        this.options.autoclose = true;
+
+      if( options.autoclose === undefined && self.options.modalType === 'alert') {
+        self.options.autoclose = true;
       }
+    } else if( self.options.autoclose && self.options.modalType === 'alert' ) {
+      fs_timeout = setTimeout(self.close, self.options.timeoutClose);
     }
+
+    return self;
 
   };
 
